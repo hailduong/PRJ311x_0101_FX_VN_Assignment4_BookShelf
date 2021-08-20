46515f18a2acf9adacae6722d0bb0b628d425e93
Ly Duong <hailyduong@gmail.com>
2021-08-20 23:33:45 +0700

    Add more UI Components

---
+++ src/com/ui/EditBook.java
@@ -0,0 +1,18 @@
+package com.ui;
+
+import javax.swing.*;
+
+public class EditBook {
+    private JPanel panel1;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JComboBox comboBox1;
+    private JList list1;
+    private JTextArea textArea1;
+    private JButton button1;
+    private JButton button2;
+    private JList list2;
+    private JButton updateButton;
+    private JButton closeButton;
+    private JButton deleteButton;
+}
---
+++ src/com/ui/MyBook.java
@@ -0,0 +1,13 @@
+package com.ui;
+
+import javax.swing.*;
+
+public class MyBook {
+    private JTable table;
+    private JPanel panel1;
+    private JPanel searchBookPanel;
+    private JLabel searchByLabel;
+    private JComboBox comboBox1;
+    private JTextField inputAnythingTextField;
+    private JButton searchButton;
+}
